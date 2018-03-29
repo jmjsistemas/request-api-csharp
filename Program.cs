62db555d12b92b535b942286aadded834d4ec980
Schio <jasane@jmjsistemas.com.br>
2018-03-29 14:09:45 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Timers;
+using RestSharp;
+using RestSharp.Authenticators;
+using static testeccsharp.TesteSimple;
+namespace testeccsharp
+{
+    class Program
+    {
+
+        
+        static string BASE_URL = "http://irishomologacao.unimeduberlandia.coop.br:8080/iris/";
+        static string CONTENT_TYPE = "application/x-www-form-urlencoded";
+        static string getSessionID()
+        {
+            var client = new RestClient(BASE_URL + "login");
+            var request = new RestRequest(Method.POST);
+
+
+            client.AddDefaultHeader("Content-Type", CONTENT_TYPE);
+            request.AddParameter("username", "appbeneficiario");
+            request.AddParameter("password", "a4997e1c241729bb32174f6a4d01a790");
+            var response = client.Execute(request);
+            Console.WriteLine(response.Cookies[0].Name + "=" + response.Cookies[0].Value);
+            return response.Cookies[0].Name + "=" + response.Cookies[0].Value;
+        }
+
+        static void getListInternacao (string sessionid) {
+
+            var client = new RestClient(BASE_URL + "api/historico-beneficiario-api/internacoes?page=0&size=1&numeroCarteira=00142104270740092");
+            var request = new RestRequest(Method.GET);
+            client.AddDefaultHeader("Content-Type","application/json:charset=UTF-8");
+            client.AddDefaultHeader("Cookie", sessionid);
+
+
+            var response =  client.Execute(request);
+            Console.WriteLine(response.Content);
+            
+           
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------------------------- \n Teste Utilizando RestSharp \n ---------------------------- - ");
+            var sessionid = getSessionID();    
+            getListInternacao(sessionid);
+
+            Console.WriteLine("----------------------------- \n Teste Simples \n ---------------------------- - ");
+
+            var id = TesteSimple.doLogin();
+            TesteSimple.doGET(id);
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
---
+++ TesteSimple.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Timers;
+namespace testeccsharp
+{
+    class TesteSimple
+    {
+        static string id = "";
+        public static string doLogin()
+        {
+            Console.WriteLine("Efetuando Login!");
+            string URI = "http://irishomologacao.unimeduberlandia.coop.br:8080/iris/login";
+            string myParameters = "username=auditor&password=da428db3b6fc127df7f3051059c7f430";
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string HtmlResult = wc.UploadString(URI, myParameters);
+                WebHeaderCollection myWebHeaderCollection = wc.ResponseHeaders;
+                // for (int i = 0; i < myWebHeaderCollection.Count; i++)
+                //    Console.WriteLine(i+" \t" + myWebHeaderCollection.GetKey(i) + " = " + myWebHeaderCollection.Get(i));
+                string[] id = myWebHeaderCollection.Get(4).Split(";");
+                Console.WriteLine("ID Retornado " + id[0]);
+
+                return id[0];
+            }
+        }
+
+        public static void doGET(string id)
+        {
+            Console.WriteLine("Efetuando GET!");
+            const string WEBSERVICE_URL = "http://irishomologacao.unimeduberlandia.coop.br:8080/iris/api/historico-beneficiario-api/internacoes?page=0&size=1&numeroCarteira=00142104270740092";
+            try
+            {
+                var webRequest = System.Net.WebRequest.Create(WEBSERVICE_URL);
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Timeout = 12000;
+                    webRequest.ContentType = "application/json";
+                    webRequest.Headers.Add("Cookie", id);
+
+                    using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+                    {
+                        using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+                        {
+                            var jsonResponse = sr.ReadToEnd();
+                            Console.WriteLine("Resultado: " + String.Format("Response: {0}", jsonResponse));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+
+
+    }
+}
